8e98d7960f3399195cf1ee1c047071c54198280a
Fabio <payoartudi@hotmail.it>
2025-02-06 19:56:11 +0100

    esercizio state

--- src/events/AlertClock.jsx
+++ src/events/AlertClock.jsx
@@ -1,6 +1,6 @@
 export const AlertClock = ({ alert }) => {
   return (
-    <div className="flex justify-center p-4">
+    <div className="p-4">
       <button
         className=" cursor-pointer text-4xl text-blue-400 bg-gray-300 rounded-xl p-6 border-2"
         onClick={alert}
---
+++ src/state/CounterDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function CounterDisplay({ count }) {
+  return (
+    <div>
+      <h2>{count}</h2>
+    </div>
+  );
+}
