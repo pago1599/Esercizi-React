fc78fe0f9337d7a7f3202938eed53745aa594ae8
Fabio <payoartudi@hotmail.it>
2025-02-07 20:31:29 +0100

    esercizio handling-events

---
+++ src/handling-events/MouseClicker.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const handleClickBtn1 = () => {
+  console.log(event.target.name);
+};
+
+const handleClickBtn2 = () => {
+  console.log(event.target.src);
+};
+
+function MouseClicker() {
+  return (
+    <div>
+      <button onClick={handleClickBtn1} name="one">
+        UNO
+      </button>
+      <hr />
+      <button name="two">
+        <img
+          onClick={handleClickBtn2}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkiUJfNHsvJD7T-imSNpnjYkZ_PKVLSuX3gw&s"
+          width={35}
+        />
+        TWO
+      </button>
+    </div>
+  );
+}
+
+export default MouseClicker;
+
+// Per far sì che in console non si stampi il valore undefined del button "TWO" ho aggiunto l'evento handler all'immagine anziché al bottone
